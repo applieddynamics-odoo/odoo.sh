c645e1c2b2e1cb8c1a266aa85147e75f4ef756de
P M <pme@odoo.com>
2022-02-25 11:57:13 -0800

    [IMP] adi_purchase_stock: added try catch and attrs removed.

--- adi_purchase_stock/models/purchase_order.py
+++ adi_purchase_stock/models/purchase_order.py
@@ -12,7 +12,10 @@ class PurchaseOrder(models.Model):
     def create(self, value):
         date_planned = value.get('date_planned')
         if date_planned:
-            dt = datetime.strptime(str(date_planned), '%Y-%m-%d %H:%M:%S')
+            try:
+                dt = datetime.strptime(date_planned, '%Y-%m-%d')
+            except:
+                dt = datetime.strptime(date_planned, '%Y-%m-%d %H:%M:%S')
             value['date_planned'] = dt.replace(hour=12, minute=00)
         res = super().create(value)
         return res
@@ -20,10 +23,21 @@ class PurchaseOrder(models.Model):
     def write(self, value):
         date_planned = value.get('date_planned')
         if date_planned:
-            dt = datetime.strptime(str(date_planned), '%Y-%m-%d %H:%M:%S')
+            try:
+                dt = datetime.strptime(date_planned, '%Y-%m-%d')
+            except:
+                dt = datetime.strptime(date_planned, '%Y-%m-%d %H:%M:%S')
+            # dt = datetime.strptime(date_planned, '%Y-%m-%d').date()
             value['date_planned'] = dt.replace(hour=12, minute=00)
         res = super().write(value)
         return res
+
+    @api.onchange('date_planned')
+    def onchange_date_planned(self):
+        if self.date_planned:
+            dt = datetime.strptime(str(self.date_planned.date()), '%Y-%m-%d')
+            date_planned = dt.replace(hour=12, minute=00)
+            self.order_line.filtered(lambda line: not line.display_type).date_planned = date_planned
 
     def button_confirm(self):
         for order in self:
