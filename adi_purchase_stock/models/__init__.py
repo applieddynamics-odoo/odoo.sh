473c051228895991f874f68aecaaf3dc5a58b609
Matthew Younger <myounger@adi.com>
2024-03-18 13:51:25 +0000

    Removed bad imports

--- adi_purchase_stock/models/__init__.py
+++ adi_purchase_stock/models/__init__.py
@@ -1,5 +1,5 @@
-from . import purchase_order
+#from . import purchase_order
 from . import res_company
 from . import res_config_settings
 from . import res_partner
-from . import stock_move
+#from . import stock_move
