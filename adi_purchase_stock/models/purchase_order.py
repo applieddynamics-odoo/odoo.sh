fe7b17e8fbce7533587e0e8dc022c5ba7d4386ba
Matthew Younger <myounger@adi.com>
2024-03-19 14:47:08 +0000

    Fix naming in SQL

--- adi_purchase_stock/models/purchase_order.py
+++ adi_purchase_stock/models/purchase_order.py
@@ -10,18 +10,18 @@ class PurchaseOrder(models.Model):
         self._cr.execute("""
         UPDATE purchase_order_line pol
             SET arrived_late = true
-        FROM purchase.order po
+        FROM purchase_order po
             WHERE                
                 pol.order_id = po.id AND 
                 (
                     po.date_order < (SELECT MAX(sp.date_done)
-                                     FROM stock_move m JOIN stock.picking sp
+                                     FROM stock_move m JOIN stock_picking sp
                                      ON m.picking_id = sp.id
                                      WHERE m.purchase_line_id = pol.id
                                      AND m.state = 'done')
                 OR
                     (pol.product_qty > (SELECT SUM(m.quantity_done)
-                                        FROM stock_move m JOIN stock.picking sp
+                                        FROM stock_move m JOIN stock_picking sp
                                         on m.picking_id = sp.id
                                         WHERE m.purchase_line_id = pol.id
                                         AND m.state = 'done')
