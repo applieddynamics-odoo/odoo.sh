9933e4a648f41ee5efccf6c06f346d5976a67420
Matthew Younger <myounger@adi.com>
2024-03-19 10:35:25 +0000

    Fix warning to set date to today correctly and reword popup

--- adi_purchase_stock/wizard/warn_effective_date.py
+++ adi_purchase_stock/wizard/warn_effective_date.py
@@ -6,5 +6,6 @@ class WarnEffectiveDate(models.TransientModel):
     _description = "Remind user to fill in the effective date if they haven't already"
 
     def button_confirm(self):
-        po_id = self.env.context.get('purchase_order_id')
-        po_id.with_context(effective_date=datetime.now().date()).button_confirm()
+        po_id = self.env['purchase.order'].search([('id', '=', self.env.context.get('purchase_order_id'))])
+        po_id['effective_date'] = datetime.now()
+        po_id.button_done()
