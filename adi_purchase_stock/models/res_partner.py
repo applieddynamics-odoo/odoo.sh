5f3b3f177fcaf0fea4e658a360084d852149903f
Matthew Younger <myounger@adi.com>
2024-03-19 15:26:47 +0000

    Reimplement filtering out office supplies and production supplies

--- adi_purchase_stock/models/res_partner.py
+++ adi_purchase_stock/models/res_partner.py
@@ -16,6 +16,16 @@ class ResPartner(models.Model):
     @api.depends('purchase_line_ids')
     def _compute_on_time_rate(self):
         for record in self:
-            self._cr.execute("SELECT AVG(1.0 - pol.arrived_late::int) FROM purchase_order_line as pol JOIN purchase_order as po ON pol.order_id = po.id WHERE pol.partner_id = %d AND po.date_order::date >= '%s'::date;" % (record.id, (datetime.now() - timedelta(365)).strftime("%Y-%m-%d")))
+            self._cr.execute(
+                """SELECT AVG(1.0 - pol.arrived_late::int)
+                FROM purchase_order_line pol JOIN purchase_order po
+                    ON pol.order_id = po.id
+                JOIN product_product p ON pol.product_id = p.id
+                JOIN product_category pc ON p.categ_id = pc.id
+                WHERE pol.partner_id = %d AND po.date_order::date >= '%s'::date
+                    AND p.detailed_type != 'service'
+                    AND pc.name != 'Office Supplies
+                    AND pc.name != 'Production Supplies';"""
+                % (record.id, (datetime.now() - timedelta(365)).strftime("%Y-%m-%d")))
             avg = self._cr.fetchone()[0]
             record['on_time_rate'] = avg * 100
