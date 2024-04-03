181cabd0920b1d981f47aeb0819281c2eb625759
Matthew Younger <myounger@adi.com>
2024-04-03 13:32:10 +0100

    change yes from 'cancel' to a custom action

--- adi_purchase_stock/wizard/warn_effective_date.py
+++ adi_purchase_stock/wizard/warn_effective_date.py
@@ -5,9 +5,12 @@ class WarnEffectiveDate(models.TransientModel):
     _name = 'warn.effective_date'
     _description = "Ensure that the delivery is correctly being marked as late/on time"
 
+    def button_nochange(self):
+        sp_id = self.env['stock.picking'].search([('id', '=', self.env.context.get('stock_picking_id'))])
+        return sp_id.with_context(skip_validation=True).button_validate()
+    
     def button_change(self):
         sp_id = self.env['stock.picking'].search([('id', '=', self.env.context.get('stock_picking_id'))])
-        raise Exception(sp_id)
         sms = self.env['stock.move'].search([('picking_id', '=', sp_id.id)])
         for m in sms:
             pl = m.purchase_line_id
