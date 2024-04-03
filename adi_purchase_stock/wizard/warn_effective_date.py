a89d0fde9317bcb014e245650b9c54adb4be5e7f
Matthew Younger <myounger@adi.com>
2024-04-03 12:40:21 +0100

    Move debug exception

--- adi_purchase_stock/wizard/warn_effective_date.py
+++ adi_purchase_stock/wizard/warn_effective_date.py
@@ -7,9 +7,9 @@ class WarnEffectiveDate(models.TransientModel):
 
     def button_change(self):
         sp_id = self.env['stock.picking'].search([('id', '=', self.env.context.get('stock_picking_id'))])
+        raise Exception(sp_id)
         sms = self.env['stock.move'].search([('picking_id', '=', sp_id.id)])
         for m in sms:
             pl = m.purchase_line_id
             pl['arrived_late'] = False
-        raise Exception(sp_id, sms)
-        sp_id.with_context(skip_validation=True).button_validate()
+        return sp_id.with_context(skip_validation=True).button_validate()
