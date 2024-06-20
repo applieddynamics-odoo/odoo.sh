263ab0d38bd745fee7fad470d4b7919db43d208d
Matthew Younger <myounger@adi.com>
2024-06-20 16:45:15 +0100

    Skip over stock pickings that aren't incoming/don't have a date done

--- adi_purchase_stock/models/stock_picking.py
+++ adi_purchase_stock/models/stock_picking.py
@@ -9,12 +9,16 @@ class StockPicking(models.Model):
         if self._context.get('skip_validation'):
             return super().button_validate()
         for r in self:
+            if r.name[3:6] == "OUT":
+                continue
             sms = self.env['stock.move'].search([('picking_id', '=', r.id)])
             #if len(sms) > 1:
             #    raise Exception("Please email myounger@adi.com with this error")
             # hopefully this works
             for sm in sms:
                 po = sm.purchase_line_id
+                if not r.date_done:
+                    continue
                 if po.date_planned.date() < r.date_done.date():
                     for m in sms:
                         pl = m.purchase_line_id
