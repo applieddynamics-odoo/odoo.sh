5e2a0813786d9ce306806df9d68d4417e132ed02
Matthew Younger <myounger@adi.com>
2024-07-31 15:14:54 +0100

    Comment out from manifest

--- ci_app_adi/__manifest__.py
+++ ci_app_adi/__manifest__.py
@@ -24,7 +24,7 @@
 
     # always loaded
     'data': [
-        'security/ir.model.access.csv',
+        #'security/ir.model.access.csv',
         'views/views.xml',
         'data/sequences.xml',
         'reports/templates.xml',
