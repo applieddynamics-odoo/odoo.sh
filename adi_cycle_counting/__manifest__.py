0229dbab5b1ce7c8c92245ae01ba2e76778238f1
Matthew Younger <myounger@adi.com>
2024-10-01 14:47:16 +0100

    etc.

--- adi_cycle_counting/__manifest__.py
+++ adi_cycle_counting/__manifest__.py
@@ -10,7 +10,7 @@
     Also adds code related to negative inventory correction
     """,
     'website':'',
-    'version':'0.1.2',
+    'version':'0.1.1',
     'author':'ADI/Matthew Younger',
     'category':'Internal Development',
 
