214c844c29e6800876aea4561307b67e292ca3ff
Matthew Younger <myounger@adi.com>
2024-10-02 13:25:36 +0100

    add adi_purchase_stock

---
+++ adi_purchase_stock/__manifest__.py
@@ -0,0 +1,31 @@
+{
+    'name':'ADI: Warning Msg Upon PO Confirmation If the On Rate is =< threshold value',
+    'summary':'ADI: Warning Msg Upon PO Confirmation If the On Rate is =< threshold value',
+    'description':
+    """
+        Task ID: 2648487
+        - Add Warning when on time percent is less than threshold	
+    """,
+    'sequence':100,
+    'license':'OPL-1',
+    'website':'https://www.adi.com',
+    'version':'1.1.1',
+    'author':'Odoo, Inc AND Matt Younger of ADI',
+    'category':'Custom Development',
+
+    # any module necessary for this one to work correctly
+    'depends': ['purchase_stock'],
+    'data': [
+        'views/purchase_order_views.xml',
+        'views/stock_move_views.xml',
+        'views/stock_picking_views.xml',
+        'views/res_config_settings_views.xml',
+        'views/res_partner_views.xml',
+        'wizard/warn_is_po_line_late_view.xml',
+        'wizard/warn_effective_date.xml',
+        'security/ir.model.access.csv',
+    ],
+    'installable':True,
+    'application':False,
+    'auto_install':False,
+}
---
+++ adi_purchase_stock/models/purchase_order.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+from odoo import api, models, fields
+
+class PurchaseOrder(models.Model):
+    _inherit = 'purchase.order'
+
+    def _approximate_on_time_rates(self):
+        self._cr.execute("UPDATE purchase_order_line SET arrived_late = false;");
+        self._cr.execute("""
+        UPDATE purchase_order_line pol
+            SET arrived_late = true
+        WHERE                
+            (SELECT BOOL_OR(date(sp.date_done) > pol.date_planned)
+                   FROM stock_move m JOIN stock_picking sp
+                   ON m.picking_id = sp.id
+                   WHERE m.purchase_line_id = pol.id
+                   AND m.state = 'done') = true
+             AND pol.product_qty > 0
+             OR (CURRENT_DATE > pol.date_planned AND pol.qty_received < pol.product_qty);
+        """)
+
+        
+class PurchaseOrderLine(models.Model):
+    _inherit = 'purchase.order.line'
+
+    arrived_late = fields.Boolean(required=True, default=lambda v: False)
+
+    def action_mark_late(self):
+        for r in self:
+            r['arrived_late'] = True
+
+    def action_unmark_late(self):
+        for r in self:
+            r['arrived_late'] = False
