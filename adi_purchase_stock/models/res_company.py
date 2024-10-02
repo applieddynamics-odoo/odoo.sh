9e3f2c069f7a7675ba5fd0db4a4ca9c5151fb7b4
Matthew Younger <myounger@adi.com>
2024-10-02 13:56:36 +0100

    remove adi_purchase_stock

--- adi_purchase_stock/models/res_company.py
+++
@@ -1,7 +0,0 @@
-from odoo import fields, models
-
-
-class Company(models.Model):
-    _inherit = 'res.company'
-
-    on_time_threshold = fields.Integer('Vendor On Time Delivery Rate')
--- adi_purchase_stock/models/res_config_settings.py
+++
@@ -1,7 +0,0 @@
-from odoo import fields, models
-
-
-class ResConfigSettings(models.TransientModel):
-    _inherit = 'res.config.settings'
-
-    on_time_threshold = fields.Integer('Vendor On Time Delivery Rate', related='company_id.on_time_threshold', readonly=False)
