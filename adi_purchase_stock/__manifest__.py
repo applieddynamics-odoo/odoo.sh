93ac0e1bcf318bf197a84093ddddcb001bfe0f33
Matthew Younger <myounger@adi.com>
2024-11-05 22:02:00 +0000

    Test keeping adi_purchase_stock un-renamed

--- adi_purchase_stock/__manifest__.py
+++ adi_purchase_stock/__manifest__.py
@@ -10,7 +10,7 @@
     'license':'OPL-1',
     'website':'https://www.adi.com',
     'version':'1.1.1',
-    'author':'Odoo, Inc AND Matt Younger of ADI',
+    'author':'Odoo, Inc AND Matthew Younger of ADI',
     'category':'Custom Development',
 
     # any module necessary for this one to work correctly
