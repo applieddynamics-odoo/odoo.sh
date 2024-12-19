695c25536ff76c930a4ddcfa841185e66a217ee8
Matthew Younger <myounger@adi.com>
2024-12-19 12:10:53 +0000

    Remove unneeded fields from ci_app_adi

--- ci_app_adi/models/models.py
+++ ci_app_adi/models/models.py
@@ -44,16 +44,17 @@ class ci_app_adi(models.Model):
     notes = fields.Text()
     actions = fields.Text()
 
+    date_due = fields.Date()
+    date_done = fields.Date()
+    
     # CI
     # not permissions-based hence different from 'date_closed'
     priority = fields.Selection([("Low",    "Low"),
                                  ("Medium", "Medium"),
                                  ("High",   "High")])
-    target_date = fields.Date()
-    date_done = fields.Date()
     
     # CAR
-    date_due = fields.Date()
+
     risk = fields.Selection([("Low",    "Low"),
                              ("Medium", "Medium"),
                              ("High",   "High")])
