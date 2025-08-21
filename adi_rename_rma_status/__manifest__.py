a91c360811e2aff5536c74c7b42faa596172104d
Matthew Younger <myounger@adi.com>
2025-08-21 11:21:11 +0100

    add rma change module

---
+++ adi_rename_rma_status/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': "Rename RMA Status 'Confirmed' to 'Received'",
+    'summary': """
+    This module renames the RMA status 'Confirmed' to 'Received'
+    """,
+    'description': """
+    This module renames the RMA status 'Confirmed' to 'Received'
+    """,
+    'author': 'M.Younger/ADI',
+    'license': 'N/A',
+    'version': '1.0.0',
+    'depends': ['repair.order'],
+    'data': [
+    ],
+    'installable': True,
+}
---
+++ adi_rename_rma_status/models/repair_order.py
@@ -0,0 +1,6 @@
+from odoo import api, models
+
+class RepairOrder(models.Model):
+    _inherit="repair.order"
+
+    state = fields.Selection([("draft", "New"), ("confirmed", "Received"), ("under_repair", "Under Repair"), ("done", "Repaired"), ("cancel", "Cancelled")])
