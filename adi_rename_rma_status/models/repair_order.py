bb3a80ae72d85f9b917ef89e750cb0bf0c82ce31
applieddynamics-odoo <57533843+applieddynamics-odoo@users.noreply.github.com>
2025-11-21 10:16:18 +0000

    Update repair_order.py

--- adi_rename_rma_status/models/repair_order.py
+++ adi_rename_rma_status/models/repair_order.py
@@ -3,4 +3,4 @@ from odoo import api, models, fields
 class RepairOrder(models.Model):
     _inherit="repair.order"
 
-    state = fields.Selection([("draft", "New"), ("confirmed", "Received"), ("under_repair", "Under Repair"), ("done", "Repaired"), ("cancel", "Cancelled")])
+    state = fields.Selection([("draft", "Draft"), ("confirmed", "Received"), ("under_repair", "In Progress"), ("done", "Complete"), ("cancel", "Cancelled")])
