45cd5f70a1cad9850632d6f7e0fe15fd0575f328
applieddynamics-odoo <57533843+applieddynamics-odoo@users.noreply.github.com>
2026-01-14 13:24:32 +0000

    Update dispose_confirm_wizard.py

--- adi_confirmation_dialog/models/dispose_confirm_wizard.py
+++ adi_confirmation_dialog/models/dispose_confirm_wizard.py
@@ -1,1 +1,32 @@
+from odoo import fields, models
+
+class DisposeConfirmWizard(models.TransientModel):
+    _name = "adi.dispose.confirm.wizard"
+    _description = "Confirm Disposal"
+
+    record_id = fields.Many2one(
+        comodel_name="x_facility_checks",
+        required=True,
+        readonly=True,
+    )
+
+    def action_yes(self):
+        self.ensure_one()
+        rec = self.record_id
+
+        rec.write({'x_studio_selection_field_387_1jaj511eb': 'status4'})
+
+        rec.message_post(
+            body=(
+                "Please confirm that the shelf life item has been suitably disposed: "
+                "<b>YES</b><br/>"
+                f"Confirmed by: {self.env.user.name}"
+            ),
+            message_type="comment",
+            subtype_xmlid="mail.mt_note",
+        )
+        return {"type": "ir.actions.act_window_close"}
+
+    def action_no(self):
+        return {"type": "ir.actions.act_window_close"}
 
