f2d0ba72eaea8714cda672ad32df1dc534474bee
applieddynamics-odoo <57533843+applieddynamics-odoo@users.noreply.github.com>
2026-02-01 17:49:16 +0000

    Fix report inheritance for adi_aged_receivables templates

--- adi_aged_receivables/models/models.py
+++ adi_aged_receivables/models/models.py
@@ -1,27 +1,13 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import models
 
-'''
-class adi_aged_receivables(models.AbstractModel):
+
+class ReportAccountAgedReceivableADI(models.AbstractModel):
     _inherit = "account.aged.receivable"
-    _description = "Aged Receivable ADI"
-    _auto = False
 
-    invoice_date = fields.Date(_compute="_compute_invoice_date")
-
-    def _compute_invoice_date(self):
-        for r in self:
-            r.journal_id.invoice_date
-
-    @api.model
-    def _get_report_name(self):
-        return _("ADI Aged Receivable")
-''''''
-    @api.model
     def _get_templates(self):
-        # OVERRIDE
-        templates = super(ReportAccountAgedReceivable, self)._get_templates()
-        templates['line_template'] = 'adi_aged_receivables.line_template_aged_receivable_report'
+        templates = super()._get_templates()
+        templates["line_template"] = "adi_aged_receivables.line_template_aged_receivable_report"
         return templates
-'''
+
