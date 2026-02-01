6f355e57812c860d778614b1936b4f3909ec3cce
applieddynamics-odoo <57533843+applieddynamics-odoo@users.noreply.github.com>
2026-02-01 18:29:34 +0000

    Fix inheritance to account.aged.receivable.report.handler (Odoo 17)

--- adi_aged_receivables/models/models.py
+++ adi_aged_receivables/models/models.py
@@ -3,11 +3,12 @@
 from odoo import models
 
 
-class ReportAccountAgedReceivableADI(models.AbstractModel):
-    _inherit = "account.aged.receivable"
+class AgedReceivableADI(models.AbstractModel):
+    _inherit = "account.aged.receivable.report.handler"
 
     def _get_templates(self):
         templates = super()._get_templates()
         templates["line_template"] = "adi_aged_receivables.line_template_aged_receivable_report"
         return templates
+
 
