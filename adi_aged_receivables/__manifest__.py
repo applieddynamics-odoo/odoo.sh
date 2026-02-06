0468b983a5d118c05358606c4dea797cd0955a79
applieddynamics-odoo <57533843+applieddynamics-odoo@users.noreply.github.com>
2026-02-06 10:37:57 +0000

    Simplify adi_aged_receivables and add tax_total support
    - Remove scaffold/demo/controllers
    - Restore standard aged receivable rendering
    - Add handler logic to populate tax_total column (blank if zero)
    - Rely on UI for column layout

--- adi_aged_receivables/__manifest__.py
+++ adi_aged_receivables/__manifest__.py
@@ -1,35 +1,14 @@
-# -*- coding: utf-8 -*-
 {
-    'name': "adi_aged_receivables",
-    'license': 'LGPL-3',
-
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
-
-    'description': """
-        Long description of module's purpose
-    """,
-
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
-    'depends': ['base', 'account', 'account_reports'],
-
-    # always loaded
-    'data': [
-        'views/views.xml',
-        'views/templates.xml',
+    "name": "adi_aged_receivables",
+    "license": "LGPL-3",
+    "summary": "Aged receivable customisations (Tax column support)",
+    "version": "17.0.1.0.0",
+    "author": "Paul Davies",
+    "depends": ["account", "account_reports"],
+    "data": [
+        # no XML needed if you add the column in the UI
     ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+    "application": False,
+    "installable": True,
 }
+
--- adi_aged_receivables/models/models.py
+++ adi_aged_receivables/models/models.py
@@ -1,14 +1,52 @@
-# -*- coding: utf-8 -*-
-
 from odoo import models
 
 
-class AgedReceivableADI(models.AbstractModel):
+class AgedReceivableTaxColumn(models.AbstractModel):
     _inherit = "account.aged.receivable.report.handler"
 
-    def _get_templates(self):
-        templates = super()._get_templates()
-        templates["line_template"] = "adi_aged_receivables.line_template_aged_receivable_report"
-        return templates
+    def _get_lines(self, options, line_id=None):
+        lines = super()._get_lines(options, line_id=line_id)
+
+        # Only act if the report includes our column
+        tax_col_idx = None
+        for i, col in enumerate(options.get("columns", [])):
+            if col.get("expression_label") == "tax_total":
+                tax_col_idx = i
+                break
+        if tax_col_idx is None:
+            return lines
+
+        MoveLine = self.env["account.move.line"]
+
+        for line in lines:
+            line_ref = line.get("id")
+
+            # Only journal item lines look like "account.move.line,123"
+            if not (isinstance(line_ref, str) and line_ref.startswith("account.move.line,")):
+                continue
+
+            try:
+                aml_id = int(line_ref.split(",")[1])
+            except Exception:
+                continue
+
+            aml = MoveLine.browse(aml_id)
+            if not aml.exists():
+                continue
+
+            move = aml.move_id
+
+            # Sum of tax journal lines on the move (company currency)
+            tax_lines = move.line_ids.filtered(lambda l: l.tax_line_id)
+            tax_total = abs(sum(tax_lines.mapped("balance")))
+
+            # Blank cell if 0
+            if not tax_total:
+                line["columns"][tax_col_idx].update({"name": "", "no_format": None})
+            else:
+                line["columns"][tax_col_idx].update({"no_format": tax_total})
+
+        return lines
+
 
 
