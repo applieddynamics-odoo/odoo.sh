47de10130c595584a25c5d2ad2fb47cc163a29b0
applieddynamics-odoo <57533843+applieddynamics-odoo@users.noreply.github.com>
2026-02-07 11:13:10 +0000

    adi_salesorder_custom: module skeleton

---
+++ adi_salesorder_custom/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "adi_salesorder_custom",
+    "license": "LGPL-3",
+    "summary": "Sales App - ADI customisation",
+    "version": "17.0.1.0.0",
+    "author": "Paul Davies",
+    "depends": ["sale", "mail"],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/sale_order_views.xml',
+    ],
+
+    "application": False,
+    "installable": True,
+}
