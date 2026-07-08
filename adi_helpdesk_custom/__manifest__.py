6c3e6bb07823ecfe693e03c020d2a25af2eebb9c
Paul Davies <pdavies@adi.com>
2026-07-08 13:39:41 +0100

    Helpdesk 2nd drop
    this version adds internal New Ticket creation, website email checking, updated ticket/customer validation logic, Set In Progress UI improvements, and related security/styling/view changes.

--- adi_helpdesk_custom/__manifest__.py
+++ adi_helpdesk_custom/__manifest__.py
@@ -18,17 +18,14 @@
         "views/review_validity_wizard_views.xml",
         "views/helpdesk_blocklist_views.xml",
         "views/confirm_email_block_wizard_views.xml",
-        "views/new_ticket_wizard_views.xml",
     ],
     "assets": {
         "web.assets_backend": [
             "adi_helpdesk_custom/static/src/scss/helpdesk_ticket.scss",
             "adi_helpdesk_custom/static/src/js/helpdesk_ticket_problem_toggle.js",
-            "adi_helpdesk_custom/static/src/js/helpdesk_ticket_new_redirect.js",
         ],
         "web.assets_frontend": [
             "adi_helpdesk_custom/static/src/js/website_form_message.js",
-            "adi_helpdesk_custom/static/src/js/website_helpdesk_email_check.js",
         ],
     },
     "installable": True,
