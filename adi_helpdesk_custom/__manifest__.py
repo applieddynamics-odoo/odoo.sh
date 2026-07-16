2012419043f6b772086cc03ea6a58b951d3e9a91
Paul Davies <pdavies@adi.com>
2026-07-16 12:52:47 +0100

    Raise RMA new view

--- adi_helpdesk_custom/__manifest__.py
+++ adi_helpdesk_custom/__manifest__.py
@@ -24,6 +24,7 @@
         "views/website_helpdesk_form_templates.xml",
         "views/closure_wizard.xml",
         "views/helpdesk_enquiry_views.xml",
+        "views/helpdesk_repair_views.xml",
     ],
     "assets": {
         "web.assets_backend": [
