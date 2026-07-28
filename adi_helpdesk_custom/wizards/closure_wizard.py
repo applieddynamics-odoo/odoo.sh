9f98851d67439d700aba614b3694ad24885f1968
Paul Davies <pdavies@adi.com>
2026-07-28 08:06:42 +0100

    Add portal summary for closure

--- adi_helpdesk_custom/wizards/closure_wizard.py
+++ adi_helpdesk_custom/wizards/closure_wizard.py
@@ -1,5 +1,8 @@
+from markupsafe import Markup, escape
+
 from odoo import fields, models
 from odoo.exceptions import UserError
+
 
 class AdiHelpdeskCloseWizard(models.TransientModel):
     _name = "adi.helpdesk.close.wizard"
@@ -24,7 +27,7 @@ class AdiHelpdeskCloseWizard(models.TransientModel):
         string="Resolution Category",
         required=True,
     )
-    
+
     adi_closure_statement = fields.Text(
         string="Closure Statement",
         required=True,
@@ -33,25 +36,75 @@ class AdiHelpdeskCloseWizard(models.TransientModel):
     def action_confirm(self):
         self.ensure_one()
 
-        closed_stage = self.env["helpdesk.stage"].search([
-            ("name", "=", "Closed")
-        ], limit=1)
+        closed_stage = self.env["helpdesk.stage"].search(
+            [("name", "=", "Closed")],
+            limit=1,
+        )
 
         if not closed_stage:
-            raise UserError("Could not find a Helpdesk stage called 'Closed'.")
+            raise UserError(
+                "Could not find a Helpdesk stage called 'Closed'."
+            )
 
-        closure_time = fields.Datetime.now()
+        ticket = self.ticket_id
 
-        self.ticket_id.write({
+        ticket.write({
             "stage_id": closed_stage.id,
             "adi_closure_result": self.adi_closure_result,
             "adi_closure_statement": self.adi_closure_statement,
+            "close_date": fields.Datetime.now(),
         })
 
-        self.ticket_id.write({
-            "close_date": closure_time,
-        })
+        closure_result_labels = dict(
+            self._fields["adi_closure_result"].selection
+        )
+        closure_result_label = closure_result_labels.get(
+            self.adi_closure_result,
+            self.adi_closure_result,
+        )
+
+        statement_lines = (
+            self.adi_closure_statement or ""
+        ).splitlines()
+
+        closure_statement_html = Markup("<br/>").join(
+            escape(line) for line in statement_lines
+        )
+
+        message_body = Markup("""
+            <div>
+                <p>
+                    <strong>Ticket Closed</strong>
+                </p>
+
+                <p>
+                    Your support request has now been completed.
+                </p>
+
+                <p>
+                    <strong>Closure result</strong><br/>
+                    {closure_result}
+                </p>
+
+                <p>
+                    <strong>Closure statement</strong><br/>
+                    {closure_statement}
+                </p>
+
+                <p>
+                    You will shortly receive an email inviting you to rate
+                    the support you received.
+                </p>
+            </div>
+        """).format(
+            closure_result=escape(closure_result_label),
+            closure_statement=closure_statement_html,
+        )
+
+        ticket.message_post(
+            body=message_body,
+            message_type="comment",
+            subtype_xmlid="mail.mt_comment",
+        )
 
         return {"type": "ir.actions.act_window_close"}
-    
-
