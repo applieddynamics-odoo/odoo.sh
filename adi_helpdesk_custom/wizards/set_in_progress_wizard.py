f60c0a15a6533e3042f354fd72a5ee9e80294cda
Paul Davies <pdavies@adi.com>
2026-07-31 09:36:59 +0100

    Update set_in_progress_wizard.py

--- adi_helpdesk_custom/wizards/set_in_progress_wizard.py
+++ adi_helpdesk_custom/wizards/set_in_progress_wizard.py
@@ -124,6 +124,13 @@ class AdiHelpdeskSetInProgressWizard(models.TransientModel):
             if company:
                 wizard.adi_charge_to_order_domain = [
                     ("partner_id", "child_of", company.id),
+                    ("state", "=", "sale"),
+                    ("x_studio_lifecycle", "=", "In Progress"),
+                    (
+                        "x_studio_sales_order_type",
+                        "in",
+                        ["Maintenance", "Maintenance Plus"],
+                    ),
                 ]
             else:
                 wizard.adi_charge_to_order_domain = [
