8094de1562057c91ba6ec30209c9df5938d7430c
Paul Davies <pdavies@adi.com>
2026-09-07 15:45:09 +0100

    Revert "Enquiry email updates"
    This reverts commit 45c2fa8e92ae077359f6f2e1e3b44c3941914cb9.

--- adi_helpdesk_custom/__manifest__.py
+++ adi_helpdesk_custom/__manifest__.py
@@ -33,7 +33,6 @@
         "views/helpdesk_portal_templates.xml",
         "views/helpdesk_website_form_templates.xml",
         "views/enquiry_complete_wizard_views.xml",
-        "views/helpdesk_enquiry_notification.xml",
     ],
     "assets": {
         "web.assets_backend": [
