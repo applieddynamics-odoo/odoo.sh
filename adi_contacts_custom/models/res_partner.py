0b822f398567e5da23734d27b1b49683cb49bfb5
Paul Davies <pdavies@adi.com>
2026-09-08 16:17:41 +0100

    Add approved helpdesk company logic

--- adi_contacts_custom/models/res_partner.py
+++ adi_contacts_custom/models/res_partner.py
@@ -4,6 +4,14 @@ from odoo import api, fields, models
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
+    adi_helpdesk_approved_company = fields.Boolean(
+        string="Helpdesk Approved Company",
+        default=False,
+        help=(
+            "Tick this box when this company is approved to use ADI Helpdesk."
+        ),
+    )
+
     @api.depends_context("adi_show_contact_name_only")
     def _compute_display_name(self):
         if not self.env.context.get("adi_show_contact_name_only"):
