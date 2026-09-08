2f0d36ced5b056679427a4ef780e2a600e172c5c
Paul Davies <pdavies@adi.com>
2026-09-08 16:30:09 +0100

    Revert "Add approved helpdesk company logic"
    This reverts commit 0b822f398567e5da23734d27b1b49683cb49bfb5.

--- adi_helpdesk_custom/wizards/new_ticket_wizard.py
+++ adi_helpdesk_custom/wizards/new_ticket_wizard.py
@@ -13,7 +13,9 @@ class AdiHelpdeskNewTicketWizard(models.TransientModel):
         domain=[
             ("is_company", "=", True),
             ("active", "=", True),
-            ("adi_helpdesk_approved_company", "=", True),
+            ("child_ids.is_company", "=", False),
+            ("child_ids.active", "=", True),
+            ("child_ids.email", "!=", False),
         ],
     )
 
